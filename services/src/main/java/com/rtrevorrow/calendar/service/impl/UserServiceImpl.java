a4fa0ae347b648a6a655228c01a85622396b4ff7
rtrevorrow <rtrevorrow@codametrix.com>
2021-02-02 19:06:30 -0500

    cleaned up code

--- services/src/main/java/com/rtrevorrow/calendar/service/impl/UserServiceImpl.java
+++ services/src/main/java/com/rtrevorrow/calendar/service/impl/UserServiceImpl.java
@@ -4,16 +4,11 @@ import com.rtrevorrow.calendar.model.User;
 import com.rtrevorrow.calendar.repository.UserRepository;
 import com.rtrevorrow.calendar.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
-
-@Service//("UserServiceImpl")
-public class UserServiceImpl implements UserDetailsService, UserService {
+@Service
+public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
@@ -22,18 +17,9 @@ public class UserServiceImpl implements UserDetailsService, UserService {
     PasswordEncoder passwordEncoder;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println("loadUserByUsername");
-        return null;
-    }
-
-    @Override
     public User createUser(User user) {
-        User dbUser = userRepository.findByUsername(user.getUsername()).orElse(null);
-
-        if (Objects.nonNull(dbUser)) {
-            throw new IllegalArgumentException("User already exists");
-        }
+        userRepository.findByUsername(user.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("User already exists"));
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
